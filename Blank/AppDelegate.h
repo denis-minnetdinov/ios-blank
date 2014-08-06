e99d6909b7fdd2d4b07d8525bcbb3381b34622a1
Denis Minnetdinov <denis.minnetdinov@flatsourcing.com>
2014-08-06 08:42:03 +0400

    Initial Commit

---
+++ Blank/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Blank
+//
+//  Created by MDR on 06.08.14.
+//  Copyright (c) 2014 Softeam LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blank/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blank
+//
+//  Created by MDR on 06.08.14.
+//  Copyright (c) 2014 Softeam LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
